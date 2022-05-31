c007fca92aa67299d3c8f4e78d8ddade0c211db8
Washington <wluizs245@gmail.com>
2022-05-30 21:57:26 -0300

    atualizações

--- src/components/NewModal.js
+++ src/components/NewModal.js
@@ -1,15 +1,41 @@
 import React, { useState } from 'react';
 import '../css/newModal.css';
 
-const maskMoney = (value) => {
-  return value
-    .replace(/\D/g, '')
-    .replace(/(\d)(\d{2})$/, '$1,$2')
-    .replace(/(?=(\d{3})+(\D))\B/g, '.');
-};
-
 function NewModal(props) {
+  
+  /*Função maskMoney formata a máscara do Modal com pontos e vírgulas*/
+  const maskMoney = (value) => {
+    return value
+      .replace(/\D/g, '')
+      .replace(/(\d)(\d{2})$/, '$1,$2')
+      .replace(/(?=(\d{3})+(\D))\B/g, '.');
+  };
+
   const [money, setMoney] = useState('');
+  const [cartaoValido, setCartaoValido] = useState('');
+
+  // pay = () => {
+  //     bodyForm = newFormData;
+  //     bodyForm.append('transactionPayLoad', JSON.stringify({
+  //        // Card Info
+  // card_number: string,
+  // cvv: number,
+  // expiry_date: string,
+
+  // // Destination User ID
+  // destination_user_id: number,
+
+  // // Value of the Transaction
+  // value: number
+
+  //     }))
+
+  //     fetch('https://run.mocky.io/v3/533cd5d7-63d3-4488-bf8d-4bb8c751c989',{
+  //       method: 'POST',
+  //       body: 
+  //     })
+  // }
+
   return (
     <>
       <div className="matrix">
@@ -32,13 +58,14 @@ function NewModal(props) {
             className="price"
             type="text"
             value={money}
+            /*chamada da função que formata o input*/
             onChange={(e) => setMoney(maskMoney(e.target.value))}
-            maxLength={30}
+            maxLength={21}
             placeholder="R$ 0,00"
           />
         </div>
         <div className="selectedCard">
-          <select className="cards">
+          <select className="cards" onchange={(e) =>(cartaoValido(e.target.value))}>
             <option className="validCard" value="1111111111111111">
               Cartão com final 1111
             </option>
@@ -48,7 +75,7 @@ function NewModal(props) {
           </select>
         </div>
         <div className="paymentButton">
-          <button className="btn-pay">Pagar</button>
+          <button className="btn-pay"  >Pagar</button>
         </div>
       </div>
     </>
